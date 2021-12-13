2d2afc55cab4cd1cb36cf7036b87f2ac2b4b456d
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-13 01:13:10 -0500

    add create new user functionality

--- chitchat/main_app/views.py
+++ chitchat/main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserRegistrForm
 
 # Create your views here.
 def home(request):
@@ -6,3 +8,17 @@ def home(request):
 
 def chitchat_index(request):
     return render(request, 'chitchat/index.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserRegistrForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserRegistrForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
