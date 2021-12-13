4e006ff36b602d63de6f1dbb3b3593a0ab837c91
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-13 01:16:14 -0500

    Implement Authorization on chitchat index

--- chitchat/main_app/views.py
+++ chitchat/main_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import UserRegistrForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render (request, 'home.html')
-
+    
+@login_required
 def chitchat_index(request):
     return render(request, 'chitchat/index.html')
 
