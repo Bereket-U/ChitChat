0137fb882c1d4533fbc4b93a01e8b5b2adf78258
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-13 20:31:14 -0500

    add new Comment

--- chitchat/main_app/forms.py
+++ chitchat/main_app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
+from .models import Comment
 
 
 class UserRegistrForm(UserCreationForm):
@@ -9,3 +11,8 @@ class UserRegistrForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+class CommentForm(ModelForm):
+  class Meta:
+    model = Comment
+    fields = ['comment']
