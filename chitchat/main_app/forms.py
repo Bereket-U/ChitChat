b638d2a61580b7f9f31f19da953f1503985a22e8
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-16 11:16:16 -0500

    forms and style update

--- chitchat/main_app/forms.py
+++ chitchat/main_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import Comment, Post
 
 
@@ -12,6 +12,15 @@ class UserRegistrForm(UserCreationForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
+        widgets = {
+          'username': forms.TextInput(attrs={'class': 'form-control'}),
+          'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+          'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+          'email': forms.TextInput(attrs={'class': 'form-control'}),
+          'password1': forms.TextInput(attrs={'class': 'form-control'}),
+          'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
 class CommentForm(ModelForm):
   class Meta:
     model = Comment
@@ -21,7 +30,7 @@ class UpdateComment(ModelForm):
   class Meta:
     model = Comment
     fields = ['comment']
-    
+
 class PostForm(ModelForm):
   class Meta:
     model = Post
